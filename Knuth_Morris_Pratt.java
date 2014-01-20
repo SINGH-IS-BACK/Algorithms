7720e420914387bce244091778a7c07541732ac7
Kamaldeep Singh Thethi <kthethi@paypal.com>
2014-01-20 11:50:08 +0530

    kmp

--- Knuth_Morris_Pratt.java
+++ Knuth_Morris_Pratt.java
@@ -2,28 +2,54 @@ import java.util.*;
 
 class Knuth_Morris_Pratt{
 
-	void Preprocessing(){
-		String Pattern = "abaababaabaaa";
-		int value[] = new int[Pattern.length()];
+	int[] Preprocessing(String Pattern){
+		int value[] = new int[Pattern.length()+1];
 
 		int j = -1;
 		value[0] = j;
-		for(int i = 1; i<value.length ; i++){
+		for(int i = 0; i<value.length-1 ; i+=0){
 			while(j>=0 &&  Pattern.charAt(i)!=Pattern.charAt(j)){
-				j=value[j];
-				//System.out.println(j);
+				j=value[j] ;
+				//System.out.println("kamal"+j + " " + i);
 			}
+			//System.out.println("kamal");
+			//System.out.println("kamal");
+			i++;
 			j++;
 			value[i]=j;
+			//System.out.println(value[i]);
 		}
 		for(int i =0 ; i<value.length ; i++){
 			System.out.println(value[i]);
 		}
+		return value;
 
 	}
+
+	boolean Search(String text, String pattern, int[] value){
+
+		int j = 0;
+		for(int i =0 ; i<text.length(); i++){
+			while(j>0 && text.charAt(i)!=pattern.charAt(j)){
+				j=value[j+1];
+			}
+			if(text.charAt(i)==pattern.charAt(j))
+				j++;
+			if(j==pattern.length())
+				return true;
+		}
+		return false;
+	}
+
+
 	public static void main(String args[]){
 		Knuth_Morris_Pratt kmp = new Knuth_Morris_Pratt();
-		kmp.Preprocessing();
+		String text = "kamaldeep abaababaabaa singh thethi";
+		String pattern = "them";
+		String Pattern = "abaababaabaaaaa";
+		int[] value = kmp.Preprocessing(Pattern);
+		boolean found = kmp.Search(text, Pattern, value);
+		System.out.println(found);
 		//for(int i=0; i<value.length ; i++){
 		//	System.out.println(value[i]);
 		//}
